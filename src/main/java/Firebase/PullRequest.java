06dd76ee42994f0c742f3f977a28c0a7c661dd00
ChristinaJean <43584851+chrpete@users.noreply.github.com>
2020-02-06 15:13:02 +0100

    [ADD], Updates database when a oull request is done #65 (#66)

--- src/main/java/Firebase/PullRequest.java
+++ src/main/java/Firebase/PullRequest.java
@@ -3,7 +3,6 @@ public class PullRequest {
 
 
         public String url;
-        public String node_id;
         public String issue_url;
         public int number;
         public String title;
@@ -12,9 +11,8 @@ public class PullRequest {
 
         }
 
-        public PullRequest(String url, String node_id, String issue_url, int number, String title) {
+        public PullRequest(String url,  String issue_url, int number, String title) {
             this.url = url;
-            this.node_id = node_id;
             this.issue_url = issue_url;
             this.number = number;
             this.title = title;
