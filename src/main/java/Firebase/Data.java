4f77496342d500ac34cf8a4e3262f5d1a85eece6
ChristinaPetersson <chrpete@kth.se>
2020-02-06 16:06:41 +0100

    [REFACTORD], Changed the order of object database #68

---
+++ src/main/java/Firebase/Data.java
@@ -0,0 +1,21 @@
+package Firebase;
+
+public class Data {
+
+    public String updated_at;
+    public String closed_at;
+    public String merged_at;
+
+    public Data() {
+
+    }
+
+    public Data(String updated_at, String closed_at, String merged_at) {
+        this.updated_at = updated_at;
+        this.closed_at = closed_at;
+        this.merged_at = merged_at;
+
+    }
+
+
+}
